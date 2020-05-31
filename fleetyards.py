f62cf1202bf2b320243669e679a3ff7dddcb1fb1
RedBlaze42 <allairehugo@gmail.com>
2020-05-31 15:13:27 +0200

    fleetyards API v1

---
+++ fleetyards.py
@@ -0,0 +1,101 @@
+import requests
+import urllib
+import json
+import dateutil.parser
+
+class FleetYardsAPI():
+
+    def __init__(self,username,password,org_name):
+        self.base_url = "https://api.fleetyards.net"
+        self.token=self._login(username,password)
+        self.org_name = org_name
+
+    def _login(self,login,password):
+        url=urllib.parse.urljoin(self.base_url,"/v1/sessions")
+        request=requests.post(url,data={"login":login,"password":password})
+
+        if request.status_code != 200:
+            raise Exception(request.text)
+
+        return request.json()["token"]
+
+    def _get(self,url):
+        url=urllib.parse.urljoin(self.base_url,url)
+        request=requests.get(url, headers={'Authorization': "Bearer {}".format(self.token)})
+        if request.status_code != 200:
+            raise Exception("Error url {} with text: {}".format(url,request.text))
+        try:
+            return request.json()
+        except json.decoder.JSONDecodeError:
+            raise Exception("Returned to docs")
+
+    @property
+    def members(self):
+        data=self._get("v1/fleets/{}/members".format(self.org_name))
+        return data
+
+    @property
+    def member_names(self):
+        data=self.members
+        return [member["username"] for member in data]
+
+    def get_hangar(self,username):
+        data=self._get("/v1/vehicles/{}".format(username))
+        hangar=list()
+        for ship in data:
+            hangar.append(Vehicle(ship))
+        return hangar
+
+    def compare_hangars(self,hangar1,hangar2):
+        additions,removals=list(),list()
+
+        #Check for removals
+        for ship in hangar1:
+            if not ship.id in [ship2.id for ship2 in hangar2]:
+                removals.append(ship)
+
+        #Check for additions
+        for ship in hangar2:
+            if not ship.id in [ship2.id for ship2 in hangar1]:
+                additions.append(ship)
+
+        return additions,removals
+
+    def compare_members(self,members1,members2):
+        additions,removals=list(),list()
+
+        #Check for removals
+        for member in members1:
+            if not member in members2:
+                removals.append(member)
+
+        #Check for additions
+        for member in members2:
+            if not member in members1:
+                additions.append(member)
+
+        return additions,removals
+
+    def get_corp_hangar(self):
+        corp_hangar = list()
+        for member in self.member_names:
+            corp_hangar+=self.get_hangar(member)
+
+class Vehicle():
+
+    def __init__(self,json_data):
+        self.data = json_data
+        self.id=self.data["id"]
+        self.custom_name=self.data["name"]
+        self.model_data=self.data["model"]
+        self.model_id=self.model_data["id"]
+        self.model_name=self.model_data["name"]
+        self.value=int(self.model_data["pledgePrice"]) if self.model_data["pledgePrice"] is not None else 0
+        self.brand=self.model_data["manufacturer"]
+        self.brand_name=self.brand["name"]
+        self.cargo=int(self.model_data["cargo"])
+        self.image_url=self.model_data["storeImage"]
+        self.medium_image_url=self.model_data["storeImageMedium"]
+        self.crew=self.model_data["maxCrew"]
+        self.size=self.model_data["size"]
+        self.updated_at=dateutil.parser.parse(self.data["updatedAt"]).astimezone(dateutil.tz.tzlocal())
